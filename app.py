36422e015e52d59b374ec031f58bd0f206960cb3
emersonsoaressc <emerson.soares.sc@gmail.com>
2025-02-27 15:04:21 -0300

    .

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 import streamlit as st
-from auth import check_session, logout
-from pages import login, dashboard, helpdesk
+from auth import check_session
+import components.sidebar as sidebar
+import pages.dashboard as dashboard
+import pages.login as login
 
-# Configuração da página
-st.set_page_config(page_title="Shopfarma - Gestão", layout="wide")
-
-# Verifica se o usuário já está logado
+# Verifica se o usuário está autenticado
 user_data = check_session()
 
-if not user_data:
-    login.show()
+if user_data:
+    st.session_state["user"] = user_data
+    sidebar.show()  # Só chama o sidebar se o usuário estiver logado
+    dashboard.show()  # Carrega o dashboard após login
 else:
-    dashboard.show()
-
-    # Gerenciar as páginas disponíveis
-    if "current_page" not in st.session_state:
-        st.session_state.current_page = "dashboard"
-
-    menu = {
-        "dashboard": dashboard.show,
-        "helpdesk": helpdesk.show
-    }
-
-    if st.session_state.current_page in menu:
-        menu[st.session_state.current_page]()
+    login.show()  # Mostra a tela de login
 
